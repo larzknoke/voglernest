6b17e6eea5be0c373d7c518cde5720e9a37c4cbf
Lars Knoke <larz@larz-mac.local>
2019-01-26 20:33:57 +0100

    SMTP ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    :address        => 'send.one.com',
+    :address        => ENV["SMTP"],
     :port           => '587',
     :authentication => :plain,
     :user_name      => ENV["ONE_USERNAME"],
