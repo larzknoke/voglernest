db70f24eb90563b01e3e0bf8423eac55aaaeeee3
Lars Knoke <larz@larz-mac.local>
2019-01-29 22:00:57 +0100

    catch 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,4 +24,18 @@ class ApplicationController < ActionController::Base
     redirect_to '/login' unless current_user
   end
 
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from ActionController::RoutingError,      with: -> { render_404  }
+    rescue_from ActionController::UnknownController, with: -> { render_404  }
+    rescue_from ActiveRecord::RecordNotFound,        with: -> { render_404  }
+    rescue_from ActionView::MissingTemplate,         with: -> { render_404  }
+  end
+
+  def render_404
+    respond_to do |format|
+      format.html { render template: 'errors/not_found', status: 404, layout: false }
+      format.all { render nothing: true, status: 404 }
+    end
+  end
+
 end
